04f334468d9f08970856f7f367df0829131b06e9
Ghost In Wires <lpcod4067@gmail.com>
2025-07-01 09:50:16 +0500

    Footer Pushed Plus One More HomeSectionCrousel Added

--- src/customer/components/Page/Homepage/HomePage.jsx
+++ src/customer/components/Page/Homepage/HomePage.jsx
@@ -1,12 +1,17 @@
 import MainCarousel from "../../HomeCrousel/MainCrousel";
 import HomeSectionCrousel from "../../HomeSectionCoursel/HomeSectionCoursel";
 import { CoatData } from "../../Data/CoatData";
+import Footer from "../../Footer/Footer";
 const HomePage = () => {
     return (
         <div>
             <MainCarousel />
             <div className="space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10">
             <HomeSectionCrousel data={CoatData}/>
+            <HomeSectionCrousel data={CoatData}/>
+            </div>
+            <div>
+                <Footer/>
             </div>
         </div>
     );
