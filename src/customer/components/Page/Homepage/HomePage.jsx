95524f435633709a24072c0b437ccfc54c2305a5
Ghost In Wires <lpcod4067@gmail.com>
2025-07-09 16:48:03 +0500

    Made The ProductDetails Card

--- src/customer/components/Page/Homepage/HomePage.jsx
+++ src/customer/components/Page/Homepage/HomePage.jsx
@@ -5,18 +5,20 @@ import Footer from "../../Footer/Footer";
 import ProductCard from "../../Products/Productcard";
 import Product from "../../Products/Product";
 import { data_set } from "../../../../Data/new_data";
+import ProductDetails from "../../ProductDetails/ProductDetails";
 
 const HomePage = () => {
     return (
         <div>
-            <MainCarousel />
-            <div className="space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10">
+            {/* <MainCarousel /> */}
+            {/* <div className="space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10">
             <HomeSectionCrousel data={CoatData} category={"Coat"}/>
             <HomeSectionCrousel data={CoatData} />
             </div>
             <div>
                 <Product product={data_set}/>
-            </div>
+            </div> */}
+            <ProductDetails/>
             <div>
                     <Footer/>
             </div>
